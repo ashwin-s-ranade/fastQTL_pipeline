a456f2d831aefe113f1f2e091ecc3e1273c147d7
ashwin-s-ranade <ashwinranade99@gmail.com>
2020-07-24 15:28:14 -0700

    add email sending functionality; now snakemake collects log in snakemake.log, and sends that log to email upon completion of job

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 #$ -V
 #$ -cwd
 
-snakemake -j 99 --cluster "qsub -l h_rt=2:00:00,h_data=8G -cwd -V "
-mail -s "snakemake finished" asr123@ucla.edu
+snakemake -j 99 --cluster "qsub -l h_rt=1:30:00,h_data=5G -cwd -V" 2> snakemake.log
+mail -s "snakemake finished" asr123@ucla.edu < snakemake.log
